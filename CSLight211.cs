d6b8dc39a8cd9413960ff230b2e22cf645c3cdc4
gabhar-01 <shevchenko.lilya@mail.ru>
2022-06-18 11:26:28 +0700

    Homework Pack

---
+++ CSLight211.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSLight211
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suitAmount = 4;
+            int valueAmount = 9;
+            Deck playingCardDeck = new Deck(suitAmount, valueAmount);
+            
+            Player player = new Player();
+
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.SetCursorPosition(35, 12);
+                Console.WriteLine("На столе лежит колода карт. Вы можете брать по одной,");
+                Console.SetCursorPosition(30, 13);
+                Console.WriteLine("пока не решите, что Вам хватит карт.\n\n");
+                Console.SetCursorPosition(70, 15);
+                Console.WriteLine("Нажмите любую клавишу для продолжения...");
+
+                Console.ReadKey();
+                Console.Clear();
+
+                Console.SetCursorPosition(40, 4);
+                Console.WriteLine("1: Взять карту.");
+                Console.SetCursorPosition(40, 5);
+                Console.WriteLine("2: Закончить раздачу.\n");
+
+                playingCardDeck.ShowDeck();
+
+                /*
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        player.TakeCard(playingCardDeck);
+                        break;
+                    case "2":
+                        Console.WriteLine("\tВы переворачиваете карты рубашкой вниз, чтобы посмотреть, что у Вас на руках.\n");
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("\tНе буянь, введи 1 или 2.");
+                        break;
+                }
+
+                player.ShowTheHand();
+                */
+                Console.ReadKey();
+            }
+        }
+    }
+
+    class Player
+    {
+        private List<Card> _hand = new List<Card>() { new Card(CardSuit.Clubs, CardValue.Ace) };
+
+        public Player ()
+        {
+            _hand.RemoveAt(0);
+        }
+
+        public void TakeCard(Deck cardDeck)
+        {
+            _hand.Add(cardDeck.TakeCard());
+        }
+
+        public void ShowTheHand ()
+        {
+            foreach (var card in _hand)
+            {
+                Console.WriteLine(card);
+            }
+        }
+    }
+
+    class Deck
+    {
+        private int _suitAmount;
+        private int _valueAmount;
+        private List<Card> _cardDeck = new List<Card>() {new Card(CardSuit.Clubs, CardValue.Ace)};
+
+        public Deck (int suitAmoint, int valueAmount)
+        {
+            _cardDeck.RemoveAt(0);
+
+            _suitAmount = suitAmoint;
+            _valueAmount = valueAmount;
+
+            for (int i = 0; i < _suitAmount; i++)
+            {
+                for (int j = 0; j < _valueAmount; j++)
+                {
+                    CardSuit suit = (CardSuit)i;
+                    CardValue value = (CardValue)j;
+
+                    Card card = new Card(suit, value);
+
+                    _cardDeck.Add(card);
+                }
+            }
+        }
+
+        public void ShowDeck ()
+        {
+            foreach (var card in _cardDeck)
+            {
+                Console.WriteLine(card);
+            }
+        }
+
+        public Card TakeCard ()
+        {
+            Random random = new Random();
+            int removebleIndex = random.Next(0,_cardDeck.Count);
+
+            Card randomCard = _cardDeck[removebleIndex];
+            _cardDeck.RemoveAt(removebleIndex);
+
+            return randomCard;
+        }
+    }
+
+
+    class Card
+    {
+        private CardSuit _suit;
+        private CardValue _value;
+
+        public Card (CardSuit suit, CardValue value)
+        {
+            _suit = suit;
+            _value = value;
+        }
+    }
+
+    enum CardSuit
+    {
+        Diamonds,
+        Clubs,
+        Hearts,
+        Spaders
+    }
+
+    enum CardValue
+    {
+        Ace,
+        King,
+        Queen,
+        Jack,
+        Ten,
+        Nine,
+        Eight,
+        Seven,
+        Six
+    }
+}
---
+++ CSLight43.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSLight43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBase playersDataBase = new DataBase();
+
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.WriteLine("Добро пожаловать на сервер.\n" +
+                    "Игроки в базе даннных:\n");
+                playersDataBase.ShowDataBase();
+
+                Console.WriteLine("\n1: Добавить игрока.\n" +
+                    "2: Забанить игрока.\n" +
+                    "3: Разбанить игрока.\n" +
+                    "4: Удалить игрока.\n" +
+                    "5: Выйти из программы.\n");
+
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        playersDataBase.AddPlayer();
+                        break;
+                    case "2":
+                        playersDataBase.BanPlayer();
+                        break;
+                    case "3":
+                        playersDataBase.UnbanPlayer();
+                        break;
+                    case "4":
+                        playersDataBase.DeletePlayer();
+                        break;
+                    case "5":
+                        Console.WriteLine("Закрытие программы...");
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Некорректный ввод. Введите число, чтобы выбрать программу.");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+
+    class DataBase
+    {
+        public List<Player> PlayersDataBase = new List<Player>();
+
+        public void ShowDataBase()
+        {
+            foreach (var player in PlayersDataBase)
+            {
+                player.ShowInfo();
+            }
+        }
+
+        public void AddPlayer ()
+        {
+            Console.Write("Введите никнейм игрока: ");
+            string playersNickname = Console.ReadLine();
+
+            Console.Write("Введите уровень игрока: ");
+            bool isCorrect = int.TryParse(Console.ReadLine(), out int playersLevel);
+
+            if (isCorrect)
+            {
+                if (playersLevel > 0)
+                {
+                    Console.WriteLine("\nИгрок добавлен.");
+                    PlayersDataBase.Add(new Player(playersNickname, playersLevel));
+                }
+                else
+                {
+                    Console.WriteLine("\nВведены некорректные данные уровня игрока. Игрок не будет добавлен.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nВведены некорректные данные уровня игрока. Игрок не будет добавлен.");
+            }
+        }
+
+        public void BanPlayer()
+        {
+            if (PlayersDataBase.Count > 0)
+            {
+                Console.Write("Введите уникальный номер игрока: ");
+                bool isCorrect = int.TryParse(Console.ReadLine(), out int playersID);
+
+                if (isCorrect)
+                {
+                    if (playersID > 0 && playersID <= PlayersDataBase.Count)
+                    {
+                        if (PlayersDataBase[playersID - 1].IsBanned)
+                        {
+                            Console.WriteLine("\nИгрок уже забанен.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nИгрок забанен.");
+                            PlayersDataBase[playersID - 1].Ban();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nТакого номера игрока не существует. ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nВведены некорректные данные уникального номера игрока. Игрок не будет забанен.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("В базе данных пока нет игроков.");
+            }
+        }
+
+        public void UnbanPlayer()
+        {
+            if (PlayersDataBase.Count > 0)
+            {
+                Console.Write("Введите уникальный номер игрока: ");
+                bool isCorrect = int.TryParse(Console.ReadLine(), out int playersID);
+
+                if (isCorrect)
+                {
+                    if (playersID > 0 && playersID <= PlayersDataBase.Count)
+                    {
+                        if (!PlayersDataBase[playersID - 1].IsBanned)
+                        {
+                            Console.WriteLine("\nИгрок не забанен.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nИгрок разбанен.");
+                            PlayersDataBase[playersID - 1].Unban();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nТакого номера игрока не существует.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nВведены некорректные данные уникального номера игрока. Игрок не будет разбанен.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("В базе данных пока нет игроков.");
+            }
+        }
+
+        public void DeletePlayer()
+        {
+            if (PlayersDataBase.Count > 0)
+            {
+                Console.Write("Введите уникальный номер игрока: ");
+                bool isCorrect = int.TryParse(Console.ReadLine(), out int playersID);
+
+                if (isCorrect)
+                {
+                    if (playersID > 0 && playersID <= PlayersDataBase.Count)
+                    {
+                        Console.WriteLine("\nИгрок удален.");
+                        PlayersDataBase.RemoveAt(playersID - 1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nТакого номера игрока не существует.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nВведены некорректные данные уникального номера игрока. Игрок не будет удален.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("В базе данных пока нет игроков.");
+            }
+        }
+    }
+
+    class Player
+    {
+        public static int IDs;
+        public bool IsBanned { get; private set; }
+        private int _ID;
+        private string _nickname;
+        private int _level;
+
+        public Player(string nickname, int level, bool isBanned = false)
+        {
+            _ID = ++IDs;
+            _nickname = nickname;
+            _level = level;
+            IsBanned = isBanned;
+        }
+
+        public void ShowInfo()
+        {
+            Console.Write(_nickname + " #" + _ID + " LVL." + _level);
+
+            if (IsBanned)
+            {
+                Console.Write("\t/BANNED");
+            }
+
+            Console.WriteLine();
+        }
+
+        public void Ban()
+        {
+            IsBanned = true;
+        }
+
+        public void Unban()
+        {
+            IsBanned = false;
+        }
+    }
+}
