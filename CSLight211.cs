2a7ec0a888db889d714433a628fbc2f7e4620993
gabhar-01 <shevchenko.lilya@mail.ru>
2022-06-18 12:40:53 +0700

    Update CSLight211.cs

--- CSLight211.cs
+++ CSLight211.cs
@@ -13,32 +13,34 @@ namespace CSLight211
             
             Player player = new Player();
 
+            Console.SetCursorPosition(35, 12);
+            Console.WriteLine("На столе лежит колода карт. Вы можете брать по одной,");
+            Console.SetCursorPosition(30, 13);
+            Console.WriteLine("пока не решите, что Вам хватит карт.\n\n");
+            Console.SetCursorPosition(70, 15);
+            Console.Write("Нажмите любую клавишу для продолжения...");
+
+            Console.ReadKey();
+            Console.Clear();
+
             bool isRunning = true;
 
             while (isRunning)
             {
-                Console.SetCursorPosition(35, 12);
-                Console.WriteLine("На столе лежит колода карт. Вы можете брать по одной,");
-                Console.SetCursorPosition(30, 13);
-                Console.WriteLine("пока не решите, что Вам хватит карт.\n\n");
-                Console.SetCursorPosition(70, 15);
-                Console.WriteLine("Нажмите любую клавишу для продолжения...");
-
-                Console.ReadKey();
-                Console.Clear();
+                ShowCardAmount(player);
 
                 Console.SetCursorPosition(40, 4);
                 Console.WriteLine("1: Взять карту.");
                 Console.SetCursorPosition(40, 5);
                 Console.WriteLine("2: Закончить раздачу.\n");
 
-                playingCardDeck.ShowDeck();
+                //playingCardDeck.ShowDeck();
 
-                /*
                 switch (Console.ReadLine())
                 {
                     case "1":
                         player.TakeCard(playingCardDeck);
+                        Console.Clear();
                         break;
                     case "2":
                         Console.WriteLine("\tВы переворачиваете карты рубашкой вниз, чтобы посмотреть, что у Вас на руках.\n");
@@ -46,12 +48,24 @@ namespace CSLight211
                         break;
                     default:
                         Console.WriteLine("\tНе буянь, введи 1 или 2.");
+                        Console.ReadKey();
+                        Console.Clear();
                         break;
                 }
+            }
 
-                player.ShowTheHand();
-                */
-                Console.ReadKey();
+            player.ShowTheHand();
+
+            Console.ReadKey();
+        }
+
+        static void ShowCardAmount (Player player)
+        {
+            Console.Write("Рука: " + player.ShowCardAmount() + " ");
+
+            for (int i = 0; i < player.ShowCardAmount(); i++)
+            {
+                Console.Write("[] ");
             }
         }
     }
@@ -65,36 +79,49 @@ namespace CSLight211
             _hand.RemoveAt(0);
         }
 
+        public int ShowCardAmount ()
+        {
+            return _hand.Count;
+        }
+
         public void TakeCard(Deck cardDeck)
         {
-            _hand.Add(cardDeck.TakeCard());
+            if (_hand.Count < cardDeck.SuitAmount * cardDeck.ValueAmount)
+            {
+                _hand.Add(cardDeck.TakeCard());
+            }
+            else
+            {
+                Console.WriteLine("\tВы взяли все карты из колоды.");
+                Console.ReadKey();
+            }
         }
 
         public void ShowTheHand ()
         {
             foreach (var card in _hand)
             {
-                Console.WriteLine(card);
+                Console.WriteLine(card.Suit + " " + card.Value);
             }
         }
     }
 
     class Deck
     {
-        private int _suitAmount;
-        private int _valueAmount;
+        public int SuitAmount { get; private set; }
+        public int ValueAmount { get; private set; }
         private List<Card> _cardDeck = new List<Card>() {new Card(CardSuit.Clubs, CardValue.Ace)};
 
         public Deck (int suitAmoint, int valueAmount)
         {
             _cardDeck.RemoveAt(0);
 
-            _suitAmount = suitAmoint;
-            _valueAmount = valueAmount;
+            SuitAmount = suitAmoint;
+            ValueAmount = valueAmount;
 
-            for (int i = 0; i < _suitAmount; i++)
+            for (int i = 0; i < SuitAmount; i++)
             {
-                for (int j = 0; j < _valueAmount; j++)
+                for (int j = 0; j < ValueAmount; j++)
                 {
                     CardSuit suit = (CardSuit)i;
                     CardValue value = (CardValue)j;
@@ -110,7 +137,7 @@ namespace CSLight211
         {
             foreach (var card in _cardDeck)
             {
-                Console.WriteLine(card);
+                Console.WriteLine(card.Suit + " " + card.Value);
             }
         }
 
@@ -129,13 +156,13 @@ namespace CSLight211
 
     class Card
     {
-        private CardSuit _suit;
-        private CardValue _value;
+        public CardSuit Suit { get; private set; }
+        public CardValue Value { get; private set; }
 
         public Card (CardSuit suit, CardValue value)
         {
-            _suit = suit;
-            _value = value;
+            Suit = suit;
+            Value = value;
         }
     }
 
